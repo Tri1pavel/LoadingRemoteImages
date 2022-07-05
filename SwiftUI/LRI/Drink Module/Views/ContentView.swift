4d1c469ab26faae22dbac0d59014f062fa1a238d
Tri1pavel <triodinp@gmail.com>
2022-07-05 21:09:58 +0300

    [SwiftUI] [task 4] Use List to represent fetched data.

--- SwiftUI/LRI/Drink Module/Views/ContentView.swift
+++ SwiftUI/LRI/Drink Module/Views/ContentView.swift
@@ -8,14 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject private var model: DrinkModel
+    @State private var showingNetworkAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List(model.items, id: \.idDrink) { item in
+            Text("\(item.strDrink)")
+        }
+        .listStyle(PlainListStyle())
+        .onAppear {
+            model.getDrinks { error in
+                if let _ = error {
+                    showingNetworkAlert.toggle()
+                }
+            }
+        }
+        .alert(model.networkError ?? String(), isPresented: $showingNetworkAlert) {
+            Button("Dismiss", role: .cancel) { }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        let model = DrinkModel(networkAPI: NetworkLayer.shared)
         ContentView()
+            .environmentObject(model)
     }
 }
